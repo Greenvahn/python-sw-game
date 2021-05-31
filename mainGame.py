4f6d059cd50f1c1f0f41293678fc9ef4cbd9ed50
Greenvahn <saulverdemartinez@gmail.com>
2021-05-31 14:34:05 +0100

    start adding images

--- mainGame.py
+++ mainGame.py
@@ -1,1 +1,22 @@
+from platform import python_branch
+from turtle import Screen
 import pygame
+from sys import exit
+
+from pygame.locals import *
+from gameObjects import * # Our own file
+
+import random
+
+
+# Init pygame
+# -----------------
+pygame.init()
+
+Screen = pygame.display.set_mode((SCREEN_WIDTH, SCREN_HEIGTH))
+background = pygame.image.load('resources/image/background.png').convert()
+
+player_rect = []
+# pygame.Rect --> pygame object for storing rectangular coordinates
+player_rect.append(pygame.Rect(0, 99, 102, 126)) 
+player_rect.append(pygame.Rect(165, 360, 102, 126))
